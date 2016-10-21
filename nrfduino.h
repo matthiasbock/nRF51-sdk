1585d3af8dd68e2e570480d5d85f1d9dc6dbfbe4
Matthias <mail@matthiasbock.net>
2016-10-21 19:35:10 +0200

    added pin config for nRFduino board

---
+++ nrfduino.h
@@ -0,0 +1,40 @@
+/**
+ * Pin mapping of the nRF51's GPIO pins
+ * on the nRFduino
+ * for Arduino-compatible shields
+ *
+ * Derived from the nRFduino schematic:
+ *  https://raw.githubusercontent.com/interoberlin/nRF51-Boards/master/nRFduino.PCB/Top.jpg
+ */
+
+#ifndef NRFDUINO_H
+#define NRFDUINO_H
+
+#define NRFDUINO_PIN_ONBOARD_LED 28
+
+#define NRFDUINO_PIN_A0    30
+#define NRFDUINO_PIN_A1    01
+#define NRFDUINO_PIN_A2    03
+#define NRFDUINO_PIN_A3    04
+#define NRFDUINO_PIN_A4    02
+#define NRFDUINO_PIN_A5    00
+
+#define NRFDUINO_PIN_IO0   29
+#define NRFDUINO_PIN_IO1   25
+#define NRFDUINO_PIN_IO2   23
+#define NRFDUINO_PIN_IO3   21
+#define NRFDUINO_PIN_IO4   15
+#define NRFDUINO_PIN_IO5   13
+#define NRFDUINO_PIN_IO6   11
+#define NRFDUINO_PIN_IO7   09
+#define NRFDUINO_PIN_IO8   07
+#define NRFDUINO_PIN_IO9   05
+#define NRFDUINO_PIN_IO10  06
+#define NRFDUINO_PIN_IO11  08
+#define NRFDUINO_PIN_IO12  10
+#define NRFDUINO_PIN_IO13  12
+
+#define NRFDUINO_PIN_SCL   16
+#define NRFDUINO_PIN_SDA   14
+
+#endif
